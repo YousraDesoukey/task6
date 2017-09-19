95e7997ba1f3287d5a6bd1b773c725e10c07e119
YousraDesoukey <yousradesoukey@hotmail.com>
2017-09-20 01:18:59 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PhotosComponent } from './photos/photos.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PhotoservicesService } from './photoservices.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PhotosComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'photos', component: PhotosComponent}, 
+      {path: 'about-us', component: AboutUsComponent}]),
+    HttpModule
   ],
-  providers: [],
+  providers: [PhotoservicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
